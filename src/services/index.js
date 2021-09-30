d17376eb6a43849a330acc153dab3d2a6edfdb44
Anand T P <anandtp00@gmail.com>
2021-09-30 16:24:30 +0530

    bug fixes

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
-const user = require('./users/users.routes')
-const authentication = require('./authentication/authentication.routes')
+const user = require('./users/users.routes');
+const authentication = require('./authentication/authentication.routes');
+const order = require('./order/order.routes')
 
 module.exports = (app) => {
     app.use('/user', user)
     app.use('/authentication', authentication)
+    app.use('/order', order)
 }
