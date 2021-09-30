639279871a227828a8b18280933e3d5ff9f5f888
Anand T P <anandtp00@gmail.com>
2021-09-30 21:20:49 +0530

    changes

--- src/services/order/order.routes.js
+++ src/services/order/order.routes.js
@@ -5,7 +5,7 @@ const { addItems, createOrder, getOrders, getSingleOrder, updateOrder } = requir
 router.post('/add-catelogue', jwtVerification, checkRole(['ADMIN']), addItems);
 router.post('/', jwtVerification, checkRole(['CUSTOMER']), createOrder);
 router.get('/:id', jwtVerification, getSingleOrder);
-router.get('/', getOrders);
-router.put('/:id', updateOrder);
+router.get('/', jwtVerification, getOrders);
+router.put('/:id', jwtVerification, checkRole(['CUSTOMER', 'ADMIN']), updateOrder);
 
 module.exports = router;
